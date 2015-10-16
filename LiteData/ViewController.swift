52889ca49021d48ee69c63febade479cbe56fc99
PC Nguyen <pc@flint.com>
2015-10-15 18:40:20 -0700

    Initial Commit

---
+++ LiteData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiteData
+//
+//  Created by PC on 10/15/15.
+//  Copyright © 2015 PC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
