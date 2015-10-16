fd1a41bfbe35bbaa1834c3952c4a80162fbe1e74
PC Nguyen <pc@flint.com>
2015-10-15 21:42:00 -0700

    Adding entity for unit test case

---
+++ LiteData/Model/UnitTest+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  UnitTest+CoreDataProperties.swift
+//  LiteData
+//
+//  Created by PC on 10/15/15.
+//  Copyright © 2015 PC. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension UnitTest {
+
+    @NSManaged var name: String?
+
+}
---
+++ LiteData/Model/UnitTest.swift
@@ -0,0 +1,16 @@
+//
+//  UnitTest.swift
+//  LiteData
+//
+//  Created by PC on 10/15/15.
+//  Copyright © 2015 PC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class UnitTest: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
