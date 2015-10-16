da2a840346d6eaaf22e1512871caf7515dc9f192
PC Nguyen <pc@flint.com>
2015-10-15 21:32:16 -0700

    api to create entity

--- Framework/NSManagedObjectContext+Lite.swift
+++ Framework/NSManagedObjectContext+Lite.swift
@@ -10,18 +10,67 @@ import Foundation
 import CoreData
 
 extension NSManagedObjectContext {
-  static let childName = "com.litedata.write.context.child"
   
-  class func childContextWithParent(parentContext: NSManagedObjectContext, name: String? = childName) -> NSManagedObjectContext {
+  class func childContextWithParent(parentContext: NSManagedObjectContext, name: String = "com.litedata.write.context.child") -> NSManagedObjectContext {
     let context = NSManagedObjectContext(concurrencyType: .PrivateQueueConcurrencyType)
+    context.name = name
     context.parentContext = parentContext
     context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
     return context
   }
+  
+  func recursiveSave() {
+    if self.hasChanges {
+      do {
+        try self.save()
+      } catch {
+        print("Error saving: \(error as NSError)")
+      }
+    }
+  }
 }
 
+// MARK: Asynchronous API
 
+extension NSManagedObjectContext {
+  
+  func createEntity<T: NSManagedObject>(entityClass: T.Type, persisted: Bool = true, setValue: ((createdEntity: T) -> ())? ) {
+    
+    self.performBlock { [weak self] in
+      self?.createEntityUnSafe(entityClass, persisted: persisted, setValue: setValue)
+    }
+  }
+}
+
+// MARK: Synchronous API
 
 extension NSManagedObjectContext {
 
+  func createEntityAndWait<T: NSManagedObject>(entityClass: T.Type, persisted: Bool = true, setValue: ((createdEntity: T) -> ())? ) {
+    
+    self.performBlockAndWait { [weak self] in
+      self?.createEntityUnSafe(entityClass, persisted: persisted, setValue: setValue)
+    }
+  }
+}
+
+// MARK: - Private
+
+extension NSManagedObjectContext {
+  
+  private func createEntityUnSafe<T: NSManagedObject>(entityClass: T.Type, persisted: Bool = true, setValue: ((createdEntity: T) -> ())? ) {
+    
+    let entityName = ClassDescriptor<T>().className
+    guard let entityDescription = NSEntityDescription.entityForName(entityName, inManagedObjectContext: self) else { return }
+    
+    let entity = T(entity: entityDescription, insertIntoManagedObjectContext: self)
+    
+    if let setValueBlock = setValue {
+      setValueBlock(createdEntity: entity)
+    }
+    
+    if persisted {
+      self.recursiveSave()
+    }
+  }
 }
